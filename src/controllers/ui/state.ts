c2da999be0ba152fd5fdd23ccb2764baf71d703f
Unhinged Creations <wordrama0@gmail.com>
2026-02-24 04:37:48 -0500

    Fix UI state controller imports; prevent empty-state 404

--- src/controllers/ui/state.ts
+++ src/controllers/ui/state.ts
@@ -1,17 +1,16 @@
 import { Response } from 'express';
-import { ApiRequest } from '../../../types';
-import { successfulResponse } from '../../../utils/responses';
-
-import stateService from '../../../services/ui/state';
+import { ApiRequest } from '../../types';
+import { successfulResponse } from '../../utils/responses';
+import stateService from '../../services/ui/state';
 
 async function getUiState(req: ApiRequest, res: Response) {
   const uiState = await stateService.getUiState(req.userId);
-  return successfulResponse(req, res, uiState || {}, '[Ui] State', 1);
+  return successfulResponse(req, res, uiState ?? {}, '[Ui] State', 1);
 }
 
 async function updateUiState(req: ApiRequest, res: Response) {
   const uiState = await stateService.updateUiState(req.userId, req.body);
-  return successfulResponse(req, res, uiState || {}, '[Ui] State Updated', 1);
+  return successfulResponse(req, res, uiState ?? {}, '[Ui] State Updated', 1);
 }
 
 export default {
