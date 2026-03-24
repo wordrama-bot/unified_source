8355925f3dad19e5995b8eac0a413ebb141651e4
Unhinged Creations <wordrama0@gmail.com>
2026-03-24 13:34:48 -0400

    Fix spelling of confetti switch

--- wordrama-v2-main/wordrama-v2-main/src/components/wordle/App.tsx
+++ wordrama-v2-main/wordrama-v2-main/src/components/wordle/App.tsx
@@ -703,7 +703,7 @@ const [playLoseSoundChristmas] = useSound('/sounds/christmas-lost.mp3', {
                   <Label htmlFor="confetti">
                     <Tooltip>
                       <TooltipTrigger className="text-left grid grid-cols-2 items-center gap-16">
-                        Show Confettis
+                        Show Confetti
                         <InfoIcon className='w-4 h-4'/>
                       </TooltipTrigger>
                       <TooltipContent>
