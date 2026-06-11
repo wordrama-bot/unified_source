07fec8833240aad54f165080fd6117589cc81834
Unhinged Creations <wordrama0@gmail.com>
2026-06-11 18:50:46 -0400

    fixing branding

--- wordrama-v2-main/wordrama-v2-main/src/components/wordle/App.tsx
+++ wordrama-v2-main/wordrama-v2-main/src/components/wordle/App.tsx
@@ -820,17 +820,16 @@ const [playLoseSoundChristmas] = useSound('/sounds/christmas-lost.mp3', {
             </SheetContent>
           </Sheet>
           
-          <div className="flex justify-center mb-4">
-            <img
-              draggable="false"
-              src="https://utfs.io/f/vieUBZcrouNZQrdaKfbRj7hpV6g4Axl20D3nvSc9I1BEkdqr"
-              alt="Wordrama Logo"
-              className="h-16 w-auto object-contain"
-            />
-          </div>
-
           <div className="mx-auto flex w-full flex-col px-1 pt-10 pb-8 sm:px-6 md:max-w-7xl lg:px-8 short:pb-2 short:pt-2">
             <div className="flex flex-col justify-center pt-6 pb-4 short:pt-2 short:pb-2">
+              <div className="flex justify-center mb-4">
+                <img
+                  draggable="false"
+                  src="https://utfs.io/f/vieUBZcrouNZQrdaKfbRj7hpV6g4Axl20D3nvSc9I1BEkdqr"
+                  alt="Wordrama Logo"
+                  className="h-16 w-auto object-contain"
+                />
+              </div>
               <Grid
                 solution={isCustom ? gameState.custom.solution : gameState.modes[gameState.gameMode][gameState.wordPack].solution}
                 guesses={isCustom ? gameState.custom.guesses : gameState.modes[gameState.gameMode][gameState.wordPack].guesses || []}
