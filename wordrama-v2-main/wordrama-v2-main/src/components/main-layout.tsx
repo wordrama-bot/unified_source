b111507ea2b1bd90336d86ebfd288c4cbb0e671c
Unhinged Creations <wordrama0@gmail.com>
2026-06-11 12:45:17 -0400

    fixing spacing

--- wordrama-v2-main/wordrama-v2-main/src/components/main-layout.tsx
+++ wordrama-v2-main/wordrama-v2-main/src/components/main-layout.tsx
@@ -46,7 +46,7 @@ export default function Layout({
           { href: "/teams", text: "Teams" },
         ]}
       />
-      <main className="w-full mt-4">
+      <main className="w-full">
         {children}
       </main>
       <Footer />
