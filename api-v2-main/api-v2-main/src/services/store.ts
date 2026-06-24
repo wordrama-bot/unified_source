ccc4e9a69798d6e6c5b1d709066b4f906261e1ae
Unhinged Creations <wordrama0@gmail.com>
2026-06-24 14:29:29 -0400

    fixing mega pack entitlement

--- api-v2-main/api-v2-main/src/services/store.ts
+++ api-v2-main/api-v2-main/src/services/store.ts
@@ -168,29 +168,36 @@ async function getStoreItems(playerId: string, filters) {
     });
 
   // TODO: REFACTOR THIS
-  const hasMegaPack = purchasedItems.find(
-    (item) => item === '3d3ff93b-65c1-4d36-902e-3a889c71ac86',
+  const megaPackId = '3d3ff93b-65c1-4d36-902e-3a889c71ac86';
+
+  const megaPackChildIds = [
+    'ab14511c-f2ac-4b16-a8ef-7cb8ed61a2cc', // 23 Letter
+    'db526774-11da-47de-b410-5b47a4168db8', // 22 Letter
+    '72215e5b-6638-4388-84bc-55dcd36c0e05', // 21 Letter
+    '6e66a620-8e17-4f75-aa0b-1c282aafb9d8', // 20 Letter
+    '1d348c05-c51e-4ea3-a888-d4823436704f', // 19 Letter
+    '425c96ab-beff-40ef-9774-feb6db135644', // 18 Letter
+    '80e197a9-0829-4074-8e85-a88e6e8b7ea0', // 17 Letter
+    '1ee2de50-072f-4718-b8ac-7663f3069f2e', // 16 Letter
+    'fef67eba-96db-4f5e-8b25-81487a1dbc9d', // 15 Letter
+    '3159552d-8c96-4bb5-aafa-ebf36aa5a2c2', // 14 Letter
+    'b8c73f14-79ad-4495-9fd9-a4be65d5fcbc', // 13 Letter
+    '7f06b10e-d52a-4ae3-b77f-a7e9a7c5e5fb', // 12 Letter
+  ];
+
+  const hasMegaPack = purchasedItems.includes(megaPackId);
+  const hasAllMegaPackChildren = megaPackChildIds.every((id) =>
+    purchasedItems.includes(id),
   );
-  const megaFiltered = hasMegaPack
-    ? filteredData.filter((item) => {
-        const packs = [
-          'ab14511c-f2ac-4b16-a8ef-7cb8ed61a2cc', //23 Letter
-          'db526774-11da-47de-b410-5b47a4168db8', //22 Letter
-          '72215e5b-6638-4388-84bc-55dcd36c0e05', //21 Letter
-          '6e66a620-8e17-4f75-aa0b-1c282aafb9d8', //20 Letter
-          '1d348c05-c51e-4ea3-a888-d4823436704f', //19 Letter
-          '425c96ab-beff-40ef-9774-feb6db135644', // 18 Letter
-          '80e197a9-0829-4074-8e85-a88e6e8b7ea0', // 17 Letter
-          '1ee2de50-072f-4718-b8ac-7663f3069f2e', // 16 Letter
-          'fef67eba-96db-4f5e-8b25-81487a1dbc9d', // 15 Letter
-          '3159552d-8c96-4bb5-aafa-ebf36aa5a2c2', // 14 Letter
-          'b8c73f14-79ad-4495-9fd9-a4be65d5fcbc', // 13 Letter
-          '7f06b10e-d52a-4ae3-b77f-a7e9a7c5e5fb', // 12 Letter
-        ];
-        if (packs.includes(item.id)) return false;
-        return true;
-      })
-    : filteredData;
+
+  const megaFiltered =
+    hasMegaPack || hasAllMegaPackChildren
+      ? filteredData.filter((item) => {
+          if (item.id === megaPackId && hasAllMegaPackChildren) return false;
+          if (megaPackChildIds.includes(item.id) && hasMegaPack) return false;
+          return true;
+        })
+      : filteredData;
 
   return changeKeys.camelCase(megaFiltered, 10);
 }
@@ -246,7 +253,7 @@ function expandBundleItemIds(itemId: string): string[] {
 }
 
 async function purchaseItemWithCoins(playerId: string, itemId: string) {
-  const itemIds = expandBundleItemIds(itemId);
+  const itemIds = [itemId];
 
   const results = [];
 
