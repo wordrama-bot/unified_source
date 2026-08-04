8122d2a8788f396ad38bbceeaabbcaa6c2acb28f
Unhinged Creations <wordrama0@gmail.com>
2026-08-04 13:46:39 -0400

    Handle UI state initialization separately

--- wordrama-v2-main/wordrama-v2-main/src/redux/ui/reducers.ts
+++ wordrama-v2-main/wordrama-v2-main/src/redux/ui/reducers.ts
@@ -5,31 +5,42 @@ import {
 
 import { initialState } from './defaultStates';
 
-function uiReducer(state: InitialState = initialState, { type, payload }: ISetUiStateAction) {
+function uiReducer(
+  state: InitialState = initialState,
+  { type, payload }: ISetUiStateAction,
+) {
   switch (type) {
+    case "SET_UI_STATE":
+      state = {
+        ...state,
+        ...payload,
+      };
+      break;
+
     case "SET_WORDLE_LEADERBOARD_UI_STATE":
       state = {
         ...state,
         leaderboard: {
           WORDLE: {
             ...state.leaderboard.WORDLE,
-            ...payload?.updatedState
-          }
-        }
-      }
+            ...payload?.updatedState,
+          },
+        },
+      };
       break;
+
     case "SET_WORDLE_GAME_UI_STATE":
       state = {
         ...state,
         wordleGame: {
           ...state.wordleGame,
-          ...payload?.updatedState
-        }
-      }
+          ...payload?.updatedState,
+        },
+      };
       break;
   }
 
   return state;
-};
+}
 
 export default uiReducer;
