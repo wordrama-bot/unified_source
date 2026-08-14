7125bc7f88cfbd9711b56e26138fd2dc81a87f53
Unhinged Creations <wordrama0@gmail.com>
2026-08-14 13:21:37 -0400

    Clarify champion challenge wording

---
+++ database-patches/20260814_update_champion_challenge_wording.sql
@@ -0,0 +1,29 @@
+update public._challenges
+set
+  description = 'Reach #1 on any Daily leaderboard',
+  how_to_get = 'Reach #1 on any Daily leaderboard'
+where id = '4a73b85a-9e40-4ed4-a55d-a7de4ce20f8a';
+
+update public._challenges
+set
+  description = 'Reach #1 on any Weekly leaderboard',
+  how_to_get = 'Reach #1 on any Weekly leaderboard'
+where id = '3beaa47f-5638-4ba2-a42a-b45f3cb5b6bf';
+
+update public._challenges
+set
+  description = 'Reach #1 on any Monthly leaderboard',
+  how_to_get = 'Reach #1 on any Monthly leaderboard'
+where id = '0af08641-fbe3-454d-b21f-8f2cbdfdb28a';
+
+update public._challenges
+set
+  description = 'Reach #1 on any Yearly leaderboard',
+  how_to_get = 'Reach #1 on any Yearly leaderboard'
+where id = '95bb2ed7-c13b-4231-9625-e403725a5693';
+
+update public._challenges
+set
+  description = 'Reach #1 on any All-Time leaderboard',
+  how_to_get = 'Reach #1 on any All-Time leaderboard'
+where id = 'eb7590cc-d2a8-4952-a5e5-17e2bbe15202';
