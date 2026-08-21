5b2036490edd4de09dfbde545c1392c6e68078d4
Unhinged Creations <wordrama0@gmail.com>
2026-08-20 23:34:32 -0400

    Remove service token access from challenges

--- api-v2-main/api-v2-main/src/routes/v3/challenges.ts
+++ api-v2-main/api-v2-main/src/routes/v3/challenges.ts
@@ -7,7 +7,7 @@ export const router = express.Router();
 
 const authedPlayer = [
   validateToken,
-  validateUserRole(["PLAYER", "STREAMER", "SERVICE_TOKEN"]),
+  validateUserRole(["PLAYER", "STREAMER"]),
 ] as const;
 
 /* Get routes */
