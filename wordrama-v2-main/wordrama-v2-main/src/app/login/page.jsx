c9e5a912599254e0e012b0dd73568f48e87b2205
Unhinged Creations <wordrama0@gmail.com>
2026-08-26 15:59:09 -0400

    Fixing reset password link

--- wordrama-v2-main/wordrama-v2-main/src/app/login/page.jsx
+++ wordrama-v2-main/wordrama-v2-main/src/app/login/page.jsx
@@ -63,6 +63,11 @@ function AuthComponent() {
                 <Auth
                   supabaseClient={supabase}
                   view={view.id}
+                  redirectTo={
+                    typeof window !== 'undefined' && view.id === 'forgotten_password'
+                      ? `${window.location.origin}/auth-recovery`
+                      : undefined
+                  }
                   magicLink={true}
                   appearance={{
                     theme: ThemeSupa,
